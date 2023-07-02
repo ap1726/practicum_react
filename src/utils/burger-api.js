987d924d5d865869d3ea771468688aa153f7b457
an <alexei1711@yandex.ru>
2023-07-02 23:57:02 +0500

    fix. add files

---
+++ src/utils/burger-api.js
@@ -0,0 +1,10 @@
+const NORMA_API = "https://norma.nomoreparties.space/api";
+
+export const checkReponse = (res) => {
+  return res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
+};
+
+export const getIngredients = () => {
+   return fetch(`${NORMA_API}/ingredients`)
+          .then(checkReponse)
+}
